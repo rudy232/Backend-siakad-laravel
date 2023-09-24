36b5b855c7c01c8b5d84393cd676a8f6c9de4736
rudy232 <rudy232@gmail.com>
2023-09-24 17:02:16 +0800

    Page Auth dan Home ready

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+    Copyright &copy; 2023 <div class="bullet"></div> @ <a href="https://jagoflutter.com">Jago Flutter</a>
+  </div>
---
+++ resources/views/components/error-footer.blade.php
@@ -0,0 +1,3 @@
+<div class="simple-footer mt-5">
+    Copyright &copy; 2023 <div class="bullet"></div> @ <a href="https://jagoflutter.com">Jago Flutter</a>
+</div>
